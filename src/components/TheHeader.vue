9bd660cfb030627df1ea818c9ec10dc6287fdb06
Luís André <andre2016999@gmail.com>
2024-10-21 21:55:15 -0300

    add TheHeader component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="header">
+        <h1>Planejamento Semanal</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .header {
+        background-color: white;
+        color: black;
+    }
+</style>
