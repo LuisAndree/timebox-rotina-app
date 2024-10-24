b7cc40cb4fcb13f474533c972f3c574a0f71fcdc
Luís André <andre2016999@gmail.com>
2024-10-23 21:37:34 -0300

    TheTr component

---
+++ src/components/TheTr.vue
@@ -0,0 +1,45 @@
+<template>
+    <tr>
+        <!-- <TheHorario></TheHorario> -->
+        <td contenteditable="true"></td>
+        <td contenteditable="true"></td>
+        <td contenteditable="true"></td>
+        <td contenteditable="true"></td>
+        <td contenteditable="true"></td>
+        <td contenteditable="true"></td>
+        <td contenteditable="true"></td>
+        <td contenteditable="true"></td>
+    </tr>
+</template>
+
+<script>
+// import TheHorario from './TheHorario.vue'
+export default {
+    name: 'TheTd',
+    components: {
+        // TheHorario,
+    },
+}
+</script>
+
+<style>
+td {
+    padding: 15px;
+    border: 1px solid #ccc;
+    text-align: center;
+    width: 12.5%;
+}
+
+td[contenteditable="true"] {
+    background-color: #f7f9fc;
+    cursor: pointer;
+}
+
+td:hover {
+    background-color: #e9ecef;
+}
+
+td:focus {
+    background-color: #d1ecf1;
+}
+</style>
